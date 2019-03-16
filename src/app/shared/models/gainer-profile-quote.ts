a9ef68ee3cfc57e4220b1c5bcf2c2b9593bb560b
AndreyYevtukhov <a.yevtukhov@gmail.com>
2019-03-16 09:26:43 +0200

    Add revision comments

--- src/app/shared/models/gainer-profile-quote.ts
+++ src/app/shared/models/gainer-profile-quote.ts
@@ -1,4 +1,7 @@
 export class GainerProfileQuote {
+  // todo rev: RV-C1 16.03.2019 08:56
+  // todo Andrey: this constructor is too complicated
+  // check https://refactoring.guru/design-patterns/catalog -> Creational patterns
   constructor(public latestVolume: number,
               public week52High: number,
               public week52Low: number,
